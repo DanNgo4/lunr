cd35a432443179ac5afec633747e2d9e64f9f9ca
Dan Ngo <danngo.au@gmail.com>
2024-09-17 11:54:54 +1000

    calibrate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen flex-col items-stretch antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen max-w-[100vw] flex-col items-stretch antialiased`}
       >
         {children}
       </body>
